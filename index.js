2e5808e62fcf322a782526cf7d7a834a2b44ffa0
Dalton Knuckles <djck244@gmail.com>
2023-03-15 23:11:09 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(unActivity = "roller-skate") {
+    return `This Saturday, I want to ${unActivity}!`;
+}
+
+function mondayWork(aActivity = "go to the office") {
+    return `This Monday, I will ${aActivity}.`;
+}
+
+function wrapAdjective(flair = "*") {
+    return function (unDef = "special") {
+        return `You are ${flair}${unDef}${flair}!`;
+    }
+}
